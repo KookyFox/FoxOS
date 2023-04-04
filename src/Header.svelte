e3fe463e93e22aee173aec78ded90f079914588e
Mati <mati.larcebeau@scenab.re>
2023-04-04 13:40:10 +0200

    first push

---
+++ src/Header.svelte
@@ -0,0 +1,47 @@
+<script>
+  // your script goes here
+</script>
+
+
+<div class="wrapperflex">
+  <button class="cross"></button>
+  <div class="title">WELCOME TO KOOKIE'S SITE!</div>
+  <button class="expand"></button>
+
+
+</div>
+
+
+<style>
+
+ .wrapperflex{
+   display: flex;
+   align-items: center;
+ }
+
+ .title{
+   flex:4;
+ }
+
+  button {
+    height : 20px;
+    width: 20px;
+    border: 1px solid black;
+    background-color : lightgray;
+    font-size: 80%;
+    color: black;
+    margin-left:10px;
+    margin-right:10px;
+    background-repeat:none;
+  }
+
+  .cross{
+    background-image: url('./assets/cross.png');
+    background-color: white;
+  }
+
+  .expand{
+    background-image: url('./assets/expand.png');
+    background-color: white;
+  }
+</style>
