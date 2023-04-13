1f59bfec260966cefb9f83cdbefdb66ddce37a11
Mati <mati.larcebeau@scenab.re>
2023-04-13 20:53:01 +0200

    pre asset boost

--- src/app/welcome.svelte
+++ src/app/welcome.svelte
@@ -1,18 +1,29 @@
-<img src="src/assets/profilepic.png">
+<div>
+  <img src="src/assets/profilepic.png">
 
-  <h1>Welcome to my lovely website!</h1>
+  <h1><u>Welcome to my lovely website!</u></h1>
 
   <p>This is my OS-style website, you can find everything about me on here, feel free to double click on the desktop icons to reopen closed windows</p>
-  <button onclick="closeWindow('window1')">OK!</button>
-
+  <p>This website contains some adult content, user discretion is advice. By proceeding on this website you agree that you are over 18 uears of age.</p>
+  <button class="windowButton" onclick="closeWindow('window1')">OK!</button>
+</div>
   <style>
+    p{
+      text-align:justify;
+    }
+    div{
+      padding:15px;
+    }
     img{
     width: 250px;
-    padding: 20px;
+    margin: 20px;
+    border: 5px double indigo;
+    border-radius: 5px;
     }
 
     h1{
     padding-top:2px;
     margin-top:5px;
+    font-size:40px;
     }
   </style>
